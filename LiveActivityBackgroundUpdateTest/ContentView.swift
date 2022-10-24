99e2e2d95390c56149cc9516bf36dcea4b72b22f
Jinyu Meng <megabitsenmzq@gmail.com>
2022-10-24 20:53:35 +0900

    Initial Commit

---
+++ LiveActivityBackgroundUpdateTest/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LiveActivityBackgroundUpdateTest
+//
+//  Created by Jinyu Meng on 2022/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LiveActivityBackgroundUpdateTest/LiveActivityBackgroundUpdateTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveActivityBackgroundUpdateTestApp.swift
+//  LiveActivityBackgroundUpdateTest
+//
+//  Created by Jinyu Meng on 2022/10/24.
+//
+
+import SwiftUI
+
+@main
+struct LiveActivityBackgroundUpdateTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
